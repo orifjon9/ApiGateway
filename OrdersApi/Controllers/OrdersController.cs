c3db4afa49f52549d7bcbfbfb4fffe5794e9c728
ORIFJON NARKULOV <orifjon9@gmail.com>
2018-10-22 17:45:42 -0600

    Initial commit, included CatalogApi and OrdersApi

---
+++ OrdersApi/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OrdersApi.Models;
+
+namespace OrdersApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+        // GET api/orders
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> Get()
+        {
+            return new List<Order>(){
+                new Order() { Id = 1, Amount = 30.95M },
+                new Order() { Id = 2, Amount = 44.99M }
+            };
+        }
+    }
+}
---
+++ OrdersApi/Models/Order.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace OrdersApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
